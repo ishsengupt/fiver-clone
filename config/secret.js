af86e228f91973c811db043ebbc6a09b9a049d88
ishsengupt <shan.engupta@gmail.com>
2020-03-03 21:32:36 -0500

    first commit

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  database: process.env.DATABASE || 'mongodb+srv://ishan:ishan1234@ishanfiverr-cvkwx.mongodb.net/test?retryWrites=true&w=majority',
+  port: process.env.PORT || 3000,
+  secret: process.env.SECRET || 'fiverclone2222',
+
+}
